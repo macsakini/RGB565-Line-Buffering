e48abc4c54c9fe1759f50e990d6e1a7b39e7733f
macsakini <macsakini@hotmail.com>
2023-04-29 15:10:19 +0300

    Initial Commit

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 The Basys FPGA with Artix 7 has only 1800Kbits of RAM, this translates to around 225Kb.
 
-However the images being captured by the OV7670 have **648px X 540px**
+However the images being captured by the OV7670 have **648px X 540px**.
+
+Since we are using RGB565 format, 2 bytes will represent a pixel. Thus we will need approximately 174960 bytes to store the 349920pixels from the image. This translates to around 175Kb. 
+
+The aim to is to start processing the image line by line to get desired results.
--- main.c
+++ main.c
@@ -8,8 +8,29 @@ void process_image();
 
 int main()
 {
-    struct rlimit rl = {32000, 32000};
+    struct rlimit rl = {1, 1};
+
     setrlimit(RLIMIT_AS, &rl);
+
+    unsigned char buffer[324];
+
+    FILE *ptr;
+
+    ptr = fopen("image_sample.bin", "rb");
+
+    fread(buffer, sizeof(buffer), 1, ptr);
+
+    for (int i = 0; i < sizeof(buffer); i++)
+        printf("%u ", buffer[i]);
 
     return 0;
 }
+
+// void process_image()
+// {
+//     FILE *write_ptr;
+
+//     write_ptr = fopen("test.bin", "wb"); // w for write, b for binary
+
+//     fwrite(buffer, sizeof(buffer), 1, write_ptr); // write 10 bytes from our buffer
+// }
